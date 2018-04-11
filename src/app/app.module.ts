33d8f2d37192270d8ecf2f0cfe136d5aca19e406
Vilius Linge <viliusling@gmail.com>
2018-04-11 17:37:05 +0300

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroService,
+    MessageService
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
